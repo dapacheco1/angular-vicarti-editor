4282d282a59ca9dfbfcd68078cbaea26d2dcfff2
Alexander Pacheco <dannypachecodev@hotmail.com>
2024-10-22 21:58:42 -0500

    fix(grid columns): fix grid columns icons

--- src/app/core/components/grid-columns/grid-columns.component.ts
+++ src/app/core/components/grid-columns/grid-columns.component.ts
@@ -10,28 +10,28 @@ import { Component } from '@angular/core';
 export class GridColumnsComponent {
   public listItems:Array<{iconUrl:string,label:string,labelUrl:string,}> = [
     {
-      iconUrl:'https://www.svgrepo.com/show/533811/donuts-cake.svg',
-      label:'Some label',
+      iconUrl:'https://www.svgrepo.com/show/413293/learn.svg',
+      label:'Investigación',
       labelUrl: 'url'
     },
     {
-      iconUrl:'https://www.svgrepo.com/show/533811/donuts-cake.svg',
-      label:'Some label',
+      iconUrl:'https://www.svgrepo.com/show/483886/brain-illustration-12.svg',
+      label:'Innovación',
       labelUrl: 'url'
     },
     {
-      iconUrl:'https://www.svgrepo.com/show/533811/donuts-cake.svg',
-      label:'Some label',
+      iconUrl:'https://www.svgrepo.com/show/307068/public-people-group-masses.svg',
+      label:'Vinculación',
       labelUrl: ''
     },
     {
-      iconUrl:'https://www.svgrepo.com/show/533811/donuts-cake.svg',
-      label:'Some label',
+      iconUrl:'https://www.svgrepo.com/show/308767/open-book-book-guide-publication.svg',
+      label:'Calendarios',
       labelUrl: 'url'
     },
     {
-      iconUrl:'https://www.svgrepo.com/show/533811/donuts-cake.svg',
-      label:'Some label',
+      iconUrl:'https://www.svgrepo.com/show/324617/wellness-healthy-health-lifestyle-well-being-health-conscious-management.svg',
+      label:'Bienestar',
       labelUrl: ''
     }
   ];
